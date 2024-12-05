d4404726d45f0a043e16a27fb578f757100b84e9
konstantin.karpyshev <konstantin.karpyshev@shopback.com>
2024-12-06 00:33:36 +0800

    Day_05_part2

--- src/main/kotlin/Day05.kt
+++ src/main/kotlin/Day05.kt
@@ -11,18 +11,63 @@ class Day05 : Base<Day05.Data, Int>(5) {
 
     override fun part1(input: Data): Int =
         input.updates.fold(0) { r, update ->
-            update.forEachIndexed { index, page ->
-                for (i1 in 0..<index) {
-                    if (input.rules.find { it.first == page && it.second == update[i1] } != null) return@fold r
-                }
-                for (i2 in index+1..update.lastIndex) {
-                    if (input.rules.find { it.second == page && it.first == update[i2] } != null) return@fold r
-                }
-            }
-            r + update[update.lastIndex / 2]
+            if (isValid(update, input.rules)) {
+                r + update[update.lastIndex / 2]
+            } else r
         }
 
-    override fun part2(input: Data): Int = 0
+    private fun isValid(update: List<Int>, rules: List<Pair<Int, Int>>): Boolean {
+        update.forEachIndexed { index, page ->
+            for (i1 in 0..<index) {
+                if (rules.find { it.first == page && it.second == update[i1] } != null) return false
+            }
+            for (i2 in index+1..update.lastIndex) {
+                if (rules.find { it.second == page && it.first == update[i2] } != null) return false
+            }
+        }
+        return true
+    }
+
+    override fun part2(input: Data): Int =
+        input.updates.fold(0) { r, update1 ->
+            var update = update1
+            var isValid = isValid(update, input.rules)
+            val notValid = !isValid
+            while (!isValid) {
+                update = fix(update, input.rules)
+                isValid = isValid(update, input.rules)
+            }
+            if (notValid) {
+                r + update[update.lastIndex / 2]
+            } else {
+                r
+            }
+        }
+
+    private fun fix(update1: List<Int>, rules: List<Pair<Int, Int>>): List<Int> {
+        val update = update1.toMutableList()
+        for (index in update.indices) {
+            val page = update[index]
+            for (i1 in 0..<index) {
+                val rule = rules.find { it.first == page && it.second == update[i1] }
+                if (rule != null) {
+                    val dd = update[i1]
+                    update[i1] = page
+                    update[index] = dd
+                    break
+                }
+            }
+            for (i2 in index+1..update.lastIndex) {
+                val rule = rules.find { it.second == page && it.first == update[i2] }
+                if (rule != null) {
+                    update[i2] = rule.second
+                    update[index] = rule.first
+                    break
+                }
+            }
+        }
+        return update
+    }
 
     override fun mapInputData(file: File): Data {
         var isRules = true
@@ -45,6 +90,6 @@ class Day05 : Base<Day05.Data, Int>(5) {
 fun main() {
     Day05().submitPart1TestInput() // 143
     Day05().submitPart1Input() // 5091
-//    Day05().submitPart2TestInput() // 9
-//    Day05().submitPart2Input() // 1835
+    Day05().submitPart2TestInput() // 123
+    Day05().submitPart2Input() // 4681
 }
