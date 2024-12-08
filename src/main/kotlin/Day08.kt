c6af6f31cc3f161d07f42645da419cdafb021abe
konstantin.karpyshev <konstantin.karpyshev@shopback.com>
2024-12-08 18:18:27 +0800

    Day_08_part2

--- src/main/kotlin/Day08.kt
+++ src/main/kotlin/Day08.kt
@@ -14,22 +14,56 @@ class Day08 : Base<List<CharArray>, Int>(8) {
                 }
             }
         }.let { list ->
-            buildList {
+            buildSet {
                 list.map { (c1, point1) ->
                     list.map { (c2, point2) ->
                         if (c1 == c2 && point1 != point2) {
-                            add(Point(point1.x - (point2.x - point1.x), point1.y - (point2.y - point1.y)))
+                            val dx = point1.x - (point2.x - point1.x)
+                            val dy = point1.y - (point2.y - point1.y)
+                            val newPoint = Point(dx, dy)
+                            if (newPoint.x in 0..input.lastIndex && newPoint.y in 0..input.first().lastIndex) {
+                                add(newPoint)
+                            }
                         }
                     }
                 }
             }
-        }
-            .filter { it.x >= 0 && it.x <= input.lastIndex && it.y >= 0 && it.y <= input.first().lastIndex }
-            .distinct()
-            .size
+        }.size
 
 
-    override fun part2(input: List<CharArray>): Int = 0
+    override fun part2(input: List<CharArray>): Int =
+        buildList {
+            input.mapIndexed { x, chars ->
+                chars.mapIndexed { y, c ->
+                    if (c != '.')
+                        add(c to Point(x, y))
+                }
+            }
+        }.let { list ->
+            buildSet {
+                list.map { (c1, point1) ->
+                    add(point1)
+                    list.map { (c2, point2) ->
+                        if (c1 == c2 && point1 != point2) {
+                            var mult = 1
+                            val dx = point1.x - (point2.x - point1.x)
+                            val dy = point1.y - (point2.y - point1.y)
+                            var newPoint = Point(dx, dy)
+
+                            while (newPoint.x in 0..input.lastIndex && newPoint.y in 0..input.first().lastIndex) {
+                                if (list.find { it.second == newPoint } == null) {
+                                    add(newPoint)
+                                }
+                                mult++
+                                val dx1 = point1.x - ((point2.x - point1.x) * mult)
+                                val dy1 = point1.y - ((point2.y - point1.y) * mult)
+                                newPoint = Point(dx1, dy1)
+                            }
+                        }
+                    }
+                }
+            }
+        }.size
 
     override fun mapInputData(file: File): List<CharArray> =
         file.readLines().map { it.toCharArray() }
@@ -38,6 +72,6 @@ class Day08 : Base<List<CharArray>, Int>(8) {
 fun main() {
     Day08().submitPart1TestInput() // 14
     Day08().submitPart1Input() // 396
-//    Day08().submitPart2TestInput() // 11387
-//    Day08().submitPart2Input() // 362646859298554
+    Day08().submitPart2TestInput() // 34
+    Day08().submitPart2Input() // 1200
 }
