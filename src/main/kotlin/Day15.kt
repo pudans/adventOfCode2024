aa319adfd25eefe4e393721b529b17d613a6bead
konstantin.karpyshev <konstantin.karpyshev@shopback.com>
2024-12-15 16:11:24 +0800

    Day_15_part1

--- src/main/kotlin/Day15.kt
+++ src/main/kotlin/Day15.kt
@@ -3,6 +3,7 @@ package day01
 import day01.utils.Direction
 import day01.utils.Direction.Companion.fromSign
 import day01.utils.Point
+import day01.utils.move
 import java.io.File
 
 class Day15 : Base<Day15.Data, Long>(15) {
@@ -15,8 +16,42 @@ class Day15 : Base<Day15.Data, Long>(15) {
     )
 
     override fun part1(input: Data): Long {
-        println(input)
-        return 0
+        var start = input.start
+        val boxes = mutableListOf<Point<Int>>()
+        boxes.addAll(input.boxes)
+
+        println("Initial:")
+        printMap(start, boxes, input.barriers)
+
+        input.moves.forEach { move ->
+            var currentPoint = start
+            val movedBoxes = mutableListOf<Point<Int>>()
+            while (true) {
+                currentPoint = currentPoint.move(move)
+                val isBarrier = input.barriers.contains(currentPoint)
+                val isBox = boxes.contains(currentPoint)
+                if (isBarrier) {
+                    break
+                } else if (isBox) {
+                    movedBoxes.add(currentPoint)
+                } else {
+                    start = start.move(move)
+                    movedBoxes.forEach { boxes.remove(it) }
+                    movedBoxes.forEach { boxes.add(it.move(move)) }
+                    break
+                }
+            }
+//            println()
+//            println("Move: $move")
+//            printMap(start, boxes, input.barriers)
+        }
+        println()
+        println("Final:")
+        printMap(start, boxes, input.barriers)
+
+        return boxes.fold(0L) { r, box ->
+            r + (box.y * 100 + box.x)
+        }
     }
 
     override fun part2(input: Data): Long = 0
@@ -53,9 +88,27 @@ class Day15 : Base<Day15.Data, Long>(15) {
     }
 }
 
+fun printMap(start: Point<Int>, boxes: List<Point<Int>>, barriers: List<Point<Int>>) {
+    val maxX = barriers.maxOf { it.x }
+    val maxY = barriers.maxOf { it.y }
+
+    for (y in 0..maxY) {
+        println()
+        for (x in 0..maxX) {
+            val point = Point(x, y)
+            when {
+                start == point -> print("@")
+                boxes.contains(point) -> print("O")
+                barriers.contains(point) -> print("#")
+                else -> print(".")
+            }
+        }
+    }
+}
+
 fun main() {
-    Day15().submitPart1TestInput() //
-//    Day15().submitPart1Input() //
+    Day15().submitPart1TestInput() // 2028 / 10092
+    Day15().submitPart1Input() // 1526673
 //    Day15().submitPart2TestInput() //
 //    Day15().submitPart2Input() //
 }
--- src/main/kotlin/utils/Point.kt
+++ src/main/kotlin/utils/Point.kt
@@ -4,3 +4,11 @@ data class Point<T>(
     val x: T,
     val y: T
 )
+
+fun Point<Int>.move(direction: Direction): Point<Int> =
+    when (direction) {
+        Direction.UP -> Point(this.x, this.y - 1)
+        Direction.DOWN -> Point(this.x, this.y + 1)
+        Direction.RIGHT -> Point(this.x + 1, this.y)
+        Direction.LEFT -> Point(this.x - 1, this.y)
+    }
