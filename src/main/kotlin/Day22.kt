118e5d587cc618af214d0aeb45b7f6ea70c48531
konstantin.karpyshev <konstantin.karpyshev@shopback.com>
2024-12-22 18:58:30 +0800

    Day_23_part2_not_finished

--- src/main/kotlin/Day22.kt
+++ src/main/kotlin/Day22.kt
@@ -26,14 +26,49 @@ class Day22 : Base<List<Long>, Long>(22) {
 
     private fun Long.prune(): Long = this.mod(16777216L)
 
-    override fun part2(input: List<Long>): Long = 0L
+    override fun part2(input: List<Long>): Long {
+
+        input.forEach { secret ->
+
+            var result = secret
+            val prices = mutableListOf<Int>()
+            val diff = mutableListOf<Int>()
+            prices.add(result.mod(10))
+
+            repeat(10) { index ->
+                val d = result * 64L
+                result = result.mix(d)
+                result = result.prune()
+
+                val d1 = result / 32
+                result = result.mix(d1)
+                result = result.prune()
+
+                val d3 = result * 2048L
+                result = result.mix(d3)
+                result = result.prune()
+
+                val price = result.mod(10)
+                prices.add(price)
+                diff.add(price - prices[index])
+            }
+
+
+            println(prices)
+            println(diff)
+            println(diff.windowed(4))
+            println("-------")
+        }
+
+        return 0L
+    }
 
     override fun mapInputData(file: File): List<Long> = file.readLines().map { it.toLong() }
 }
 
 fun main() {
-    Day22().submitPart1TestInput() // 37327623
-    Day22().submitPart1Input() // 20071921341
-//    Day22().submitPart2TestInput() //
+//    Day22().submitPart1TestInput() // 37327623
+//    Day22().submitPart1Input() // 20071921341
+    Day22().submitPart2TestInput() //
 //    Day22().submitPart2Input() //
 }
