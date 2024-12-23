63f268b1331a9fdb1b2fc71932b21a7df4641b84
konstantin.karpyshev <konstantin.karpyshev@shopback.com>
2024-12-23 15:57:27 +0800

    Day_23_part1

--- src/main/kotlin/Day23.kt
+++ src/main/kotlin/Day23.kt
@@ -4,7 +4,25 @@ import java.io.File
 
 class Day23 : Base<List<Pair<String, String>>, Int>(23) {
 
-    override fun part1(input: List<Pair<String, String>>): Int = 0
+    override fun part1(input: List<Pair<String, String>>): Int {
+        val map = mutableMapOf<String, MutableList<String>>()
+        input.forEach {
+            map[it.first] = map.getOrDefault(it.first, mutableListOf()).apply { add(it.second) }
+            map[it.second] = map.getOrDefault(it.second, mutableListOf()).apply { add(it.first) }
+        }
+        val connected = mutableSetOf<Set<String>>()
+        map.forEach { (t1, values) ->
+            values.forEach { t2 ->
+                map[t2]!!.forEach { t3 ->
+                    if (values.contains(t3)) {
+                        connected.add(setOf(t1, t2, t3))
+                    }
+                }
+            }
+        }
+        val filtered = connected.filter { it.any { it.startsWith("t") } }
+        return filtered.size
+    }
 
     override fun part2(input: List<Pair<String, String>>): Int = 0
 
@@ -13,8 +31,8 @@ class Day23 : Base<List<Pair<String, String>>, Int>(23) {
 }
 
 fun main() {
-    Day23().submitPart1TestInput() //
-//    Day23().submitPart1Input() //
+    Day23().submitPart1TestInput() // 7
+    Day23().submitPart1Input() // 1000
 //    Day23().submitPart2TestInput() //
 //    Day22().submitPart2Input() //
 }
