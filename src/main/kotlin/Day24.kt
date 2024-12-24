a0b131c78e129311155aba2f79e36dbd98eaa871
konstantin.karpyshev <konstantin.karpyshev@shopback.com>
2024-12-24 22:50:45 +0800

    Day_24_part1

--- src/main/kotlin/Day24.kt
+++ src/main/kotlin/Day24.kt
@@ -3,7 +3,7 @@ package day01
 import day01.Day24.Operation.*
 import java.io.File
 
-class Day24 : Base<Day24.Data, Int>(24) {
+class Day24 : Base<Day24.Data, Long>(24) {
 
     data class Data(
         val initialMap: Map<String, Boolean>,
@@ -26,12 +26,28 @@ class Day24 : Base<Day24.Data, Int>(24) {
         val operation: Operation
     )
 
-    override fun part1(input: Data): Int {
-        println(input)
-        return 0
+    override fun part1(input: Data): Long {
+        val map = input.initialMap.toMutableMap()
+        fun String.getValue(): Boolean = when {
+            map.contains(this) -> map[this]!!
+            else -> {
+                val operation = input.operations[this]!!
+                val result = operation.operation.invoke(operation.value1.getValue(), operation.value2.getValue())
+                map[this] = result
+                result
+            }
+        }
+        return input.operations.keys
+            .asSequence()
+            .filter { it.startsWith("z") }
+            .map { Pair(it, it.getValue()) }
+            .sortedByDescending { it.first }
+            .map { if (it.second) "1" else "0" }
+            .joinToString("") { it }
+            .toLong(2)
     }
 
-    override fun part2(input: Data): Int = 0
+    override fun part2(input: Data): Long = 0
 
     override fun mapInputData(file: File): Data {
         val initialMap = mutableMapOf<String, Boolean>()
@@ -43,7 +59,7 @@ class Day24 : Base<Day24.Data, Int>(24) {
             if (line.isEmpty()) {
                 isOperations = true
             } else if (!isOperations) {
-                line.split(": ").let { initialMap[it.first()] = it[1] == "1"}
+                line.split(": ").let { initialMap[it.first()] = it[1] == "1" }
             } else {
                 line.split(" ").let {
                     operations[it.last()] = Connection(
@@ -58,8 +74,8 @@ class Day24 : Base<Day24.Data, Int>(24) {
 }
 
 fun main() {
-    Day24().submitPart1TestInput() //
-//    Day24().submitPart1Input() //
+    Day24().submitPart1TestInput() // 2024
+    Day24().submitPart1Input() //
 //    Day24().submitPart2TestInput() //
 //    Day24().submitPart2Input() //
 }
